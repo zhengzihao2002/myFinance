77957357c7e23e7bb37c6e0de85da0b5659d9f2c
郑子豪 <alvinzheng2002@icloud.com>
2025-11-13 20:57:29 -0500

    update runable file sh

--- scripts/git-local-ignore.sh
+++ scripts/git-local-ignore.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-git update-index --assume-unchanged userData/backup*
-git update-index --assume-unchanged userData/data.json
-git update-index --assume-unchanged userData/prepay_schedule.json
-git update-index --assume-unchanged userData/recentTransactions.json
-git update-index --assume-unchanged userData/checkingHistory.txt
-git update-index --assume-unchanged node_modules/
-git update-index --assume-unchanged dist/
-git update-index --assume-unchanged .env
-git update-index --assume-unchanged .DS_Store
+git update-index --skip-worktree userData/data.json
+git update-index --skip-worktree userData/prepay_schedule.json
+git update-index --skip-worktree userData/recentTransactions.json
+git update-index --skip-worktree userData/checkingHistory.txt
+git update-index --skip-worktree package-lock.json
+
