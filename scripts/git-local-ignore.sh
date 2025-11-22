5e7701f8589a0dc9af200ce9953e69a9b7070d07
郑子豪 <alvinzheng2002@icloud.com>
2025-11-22 17:53:35 -0500

    Added new category and made sure npm run migrate works in root

--- scripts/git-local-ignore.sh
+++ scripts/git-local-ignore.sh
@@ -3,5 +3,6 @@ git update-index --skip-worktree userData/data.json
 git update-index --skip-worktree userData/prepay_schedule.json
 git update-index --skip-worktree userData/recentTransactions.json
 git update-index --skip-worktree userData/checkingHistory.txt
+git update-index --skip-worktree userData/categories.json
 git update-index --skip-worktree package-lock.json
 
